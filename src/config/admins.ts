502387f514e165b6c37a59b1ba2f5b295bd75ada
Yhoveyker Diaz <dzyho@example.com>
2026-03-06 19:57:31 -0500

    Admin Added

--- src/config/admins.ts
+++ src/config/admins.ts
@@ -4,8 +4,12 @@
 export const AUTHORIZED_ADMINS = [
   "dzyhoveyker@gmail.com",
   "danirojascrronaldo3@gmail.com",
+  "konolas0123@gmail.com",
+  "nefokun@gmail.com",
   "1777d137-5070-4e39-9a76-40b62b91cd06",
-  "e5f2feca-79b9-4a09-9502-708055795dc2"
+  "e5f2feca-79b9-4a09-9502-708055795dc2",
+  "b3bbdd1c-9a72-471f-9fad-5ce0119fdcb7",
+  "0042f343-3964-430c-9eff-d10d91fca491",
    // Add your email here
   // You can also add Supabase User IDs
 ];
